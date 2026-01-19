7424d17d71d68f3e6571ed47badb9fb1ca0a4d86
Vincent Brodin <vincent.brodin21@gmail.com>
2026-01-19 20:15:02 +0100

    first commit

---
+++ MauiPascal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiPascal
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiPascal/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiPascal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
